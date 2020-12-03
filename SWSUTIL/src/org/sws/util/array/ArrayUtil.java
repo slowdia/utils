3ef160aade9994c6ea2141d237a1a18a5d4523ab
ago <ago@DESKTOP-QEI40R5>
2020-12-04 00:36:40 +0900

    순열 메소드 추가

---
+++ SWSUTIL/src/org/sws/util/array/ArrayUtil.java
@@ -0,0 +1,39 @@
+package org.sws.util.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ArrayUtil {
+
+	/**
+	 * 순열 리스트
+	 * @param prefix 빈 배열 new String[0]
+	 * @param array 조합 할 배열 String[]
+	 * @param list 결과 저장 할 리스트
+	 */
+	public static void Premutation(String[] prefix, String[] array, List<String[]> list){
+		if(prefix != null && prefix.length > 0){
+			list.add(prefix);
+		}
+		for(int i=0; i<array.length; i++){
+			String[] newprefix = new String[prefix.length+1];
+			System.arraycopy(prefix, 0, newprefix, 0, prefix.length);
+			newprefix[newprefix.length-1] = array[i];
+			String[] newArray = new String[array.length-1];
+			System.arraycopy(array, 0, newArray, 0, i);
+			System.arraycopy(array, i+1, newArray, i, newArray.length - i);
+			Premutation(newprefix, newArray, list);
+		}
+	}
+	
+	public static void main(String[] args){
+		List<String[]> list = new ArrayList<>();
+		String[] array = {"a", "b", "c", "d"};
+		ArrayUtil.Premutation(new String[0], array, list);
+		for(String[] arr : list){
+			System.out.println(Arrays.stream(arr).collect(Collectors.joining(", ")));
+		}
+	}
+}
